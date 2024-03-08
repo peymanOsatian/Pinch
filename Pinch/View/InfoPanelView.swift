1536bd523f68c6afbddac505a7be73fec49412f2
Peyman <peyman.osatian@gmail.com>
2024-03-08 02:18:29 +0100

    xcode

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Peyman Osatian on 2024-03-08.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoPanelView()
+}
