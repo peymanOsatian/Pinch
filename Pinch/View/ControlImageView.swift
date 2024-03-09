39381f088e0f47a99f4aded1cf73d403f5094147
Peyman <peyman.osatian@gmail.com>
2024-03-09 23:03:42 +0100

    Xcode9

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Peyman Osatian on 2024-03-09.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlImageView()
+}
