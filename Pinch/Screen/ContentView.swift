592f4bc483e9097064b171ebc8b0f4bd5f27f698
Peyman <peyman.osatian@gmail.com>
2024-03-12 00:14:34 +0100

    zcode12mar

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     //
     @State private var imageScale : CGFloat = 1
     @State private var imageOffset :CGSize = .zero
+    @State private var isDrawerOpen : Bool = false
     
     
     
@@ -30,6 +31,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             ZStack{
+                Color.clear
              // MARK: -PAGE IMAGE
                 //: shows image (modify)
                 Image("magazine-front-cover")
@@ -68,6 +70,26 @@ struct ContentView: View {
                             }
                         }
                     )
+                // MARK: -3.MAGNIFICATION
+                    .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)){
+                                if imageScale >= 1 && imageScale <= 5 {
+                                    imageScale = value
+                                } else if  imageScale > 5 {
+                                    imageScale = 5
+                                }
+                            }
+                        }
+                        .onEnded{ _ in
+                            if imageScale > 5 {
+                                imageScale = 5
+                            } else if  imageScale <= 1{
+                                resetImageSate()
+                            }
+                        }
+                    )
             }//:ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -78,6 +100,93 @@ struct ContentView: View {
                 }
             })
             //: end animation
+            // MARK: -INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+                ,alignment : .top
+            )
+                
+         // MARK: - CONTROLS
+            .overlay(
+                Group {
+                    HStack{
+                      // SCALE  DOWN
+                        Button {
+                            withAnimation(.spring()){
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    if imageScale <= 1{
+                                        resetImageSate()
+                                    }
+                                }
+                            }
+                        } label: {
+                          ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        // RESET
+                        Button {
+                          resetImageSate()
+                        } label: {
+                          ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        // SCALE UP
+                        Button {
+                            withAnimation(.spring()){
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    if imageScale > 5{
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                          ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    }//: CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            )
+            // MARK: -DRAWER
+            .overlay (
+                HStack (spacing : 12){
+                    // MARK: -DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ?  "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture(perform: {
+                            withAnimation(.easeOut){
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    
+                    // MARK: -THUMBNAILS
+                Spacer()
+                } // : drawer
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width:260)
+                    .padding(.top,UIScreen.main.bounds.height/12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment:.topTrailing
+                
+                
+            )
         }//: NAVIGATION
         .navigationViewStyle(.stack)
        
